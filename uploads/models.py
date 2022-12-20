52076b77ab38c873c46e605039adfb49434807c9
mohamed <hamood_shaheen@outlook.com>
2022-12-21 01:30:03 +0200

    create upload model

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+ACTION_CHOICES= (
+    ('NO_FILTER', 'no filter'),
+    ('COLORIZED', 'colorized'),
+    ('GRAYSCALE', 'grayscale'),
+    ('BLURRED', 'blurred'),
+    ('BINARY', 'binary'),
+    ('INVERT', 'invert'),
+)
+
+class Upload(models.Model):
+    image = models.ImageField(upload_to='images/')
+    action = models.CharField(max_length=50, choices=ACTION_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return str(self.id)
+
+
+    def save(self, *args, **kwargs):
+        pass      
